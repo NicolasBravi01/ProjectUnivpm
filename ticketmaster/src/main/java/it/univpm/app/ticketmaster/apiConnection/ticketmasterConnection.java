f502fa84dcb9b25342d26714f4e791dec1c76502
Kevin Javier <sup3rk24@gmail.com>
2021-12-16 12:15:41 +0100

    Ultimata la classe che si occupa della connessione con l'api

--- ticketmaster/src/main/java/it/univpm/app/ticketmaster/apiConnection/ticketmasterConnection.java
+++ ticketmaster/src/main/java/it/univpm/app/ticketmaster/apiConnection/ticketmasterConnection.java
@@ -2,14 +2,13 @@ package it.univpm.app.ticketmaster.apiConnection;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Vector;
+import java.net.URLConnection;
 
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
+import org.json.simple.JSONValue;
 import org.json.simple.parser.ParseException;
 
 import it.univpm.app.ticketmaster.scanner.ApiKeyReader;
@@ -19,40 +18,47 @@ public class ticketmasterConnection
 {
 	final static String countryCode = "US";		//Eventi US, United States	
 	
-	public static JSONObject getJSONEvents() {
+	public static JSONObject getJSONEvents() 
+	{
 		
 		String urlApiConnection = "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=" + countryCode + "&apikey=" + ApiKeyReader.getApiKey();	
 		JSONObject obj = null;
 		
-		try 
-		{			
-			URL url = new URL(urlApiConnection);			
-			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-					
-			//json e' una stringa con all'interno tutto il file JSON degli eventi, va convertito in jsonObject e poi messo in lista
-			BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));			
-			String json = input.readLine();		
-			
-			JSONParser parser = new JSONParser();  		
-			obj = (JSONObject) parser.parse(json);
-			
-		} 
-		catch (ParseException e) {			
-			e.printStackTrace();
-		}
-		catch (MalformedURLException e) {
-			e.printStackTrace();
-		} 
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-				
-		
-		return obj;	
-	}
-	
-	
-	
-	
+		try
+		{
+			URLConnection openConnection = new URL(urlApiConnection).openConnection();
+			InputStream in = openConnection.getInputStream();
 
+			String data = "";
+			String line = "";
+			try 
+			{
+				InputStreamReader inR = new InputStreamReader( in );
+				BufferedReader buf = new BufferedReader( inR );
+
+				while ( ( line = buf.readLine() ) != null ) {
+				data+= line;
+				}
+			} 
+			finally 
+			{
+			in.close();
+			}
+		
+			obj = (JSONObject) JSONValue.parseWithException(data);	 //parse JSON Object
+
+		} 
+		
+		catch (IOException | ParseException e) 
+		{
+		e.printStackTrace();
+		} 
+		catch (Exception e) 
+		{
+		e.printStackTrace();
+		}			
+				
+		return obj;	
+		
+	}
 }
