83589995525b308d4156be8251926a32787d45f5
Kevin Javier <sup3rk24@gmail.com>
2022-01-12 00:30:33 +0100

    Aggiunto test JUnit

---
+++ ticketmaster/src/test/java/it/univpm/app/ticketmaster/JSONBuilder/JSONBuilderTest.java
@@ -0,0 +1,94 @@
+package it.univpm.app.ticketmaster.JSONBuilder;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+import java.util.Vector;
+
+import org.json.simple.JSONObject;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.univpm.app.ticketmaster.JSONHandler.JSONBuilder;
+import it.univpm.app.ticketmaster.model.Event;
+
+class JSONBuilderTest 
+{
+	JSONObject obj = new JSONObject();
+	Vector<Event> events = new Vector<Event>();
+	JSONBuilder jb = new JSONBuilder();
+	Event event1;
+	Event event2;
+	Event event3;
+	String localDate1, localDate2, localDate3;
+	LocalDate locDt1, locDt2, locDt3;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception 
+	{
+		localDate1 = "2022-01-25";
+		locDt1 = LocalDate.parse(localDate1);
+		
+		localDate2 = "2022-03-24";
+		locDt2 = LocalDate.parse(localDate2);
+		
+		localDate3 = "2022-05-18";
+		locDt3 = LocalDate.parse(localDate3);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+	
+	
+	@Test
+	void testGetJSONObjectEvents() 
+	{
+		
+		
+		event1 = new Event("Brooklyn Nets vs. Los Angeles Lakers",
+						   "https://www.ticketmaster.com/event/Z7r9jZ1AdFUj9",
+                           "19:30:00",
+                           locDt1,
+                           "Barclays Center",
+                           "Brooklyn",
+                           "New York",
+                           "Sports",
+                           "Basketball");
+                           
+		event2 = new Event("Golden State Warriors vs. Phoenix Suns",
+	                       "https://www.ticketmaster.com/golden-state-warriors-vs-phoenix-suns-san-francisco-california-03-30-2022/event/1C005B12A59E3CBB",
+	                       "19:00:00",
+	                       locDt2,
+				           "Chase Center",
+				           "San Francisco",
+				           "California",
+				           "Sports",
+				           "Basketball");
+		
+		event3 = new Event("Phoenix Suns vs. Indiana Pacers",
+	                       "https://www.ticketmaster.com/phoenix-suns-vs-indiana-pacers-phoenix-arizona-01-22-2022/event/19005B13479C3E4B",
+	                       "19:00:00",
+	                       locDt3,
+	                       "Footprint Center",
+	                       "Phoenix",
+	                       "Arizona",
+	                       "Sports",
+	                       "Basketball");
+		
+		events.add(event1);
+		events.add(event2);
+		events.add(event3);
+		
+		obj = jb.getJSONObjectEvents(events);
+		
+		assertEquals(obj.get("number events"), 3);
+	}
+
+}
